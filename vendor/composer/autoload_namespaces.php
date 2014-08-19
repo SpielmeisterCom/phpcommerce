b603799e2005ad45e8a2bee73b7a6eeb64e8fe40
Julian Haupt <julian.haupt@abfallscout.de>
2014-08-19 14:28:13 +0200

    added more dependencies

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -7,6 +7,10 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Symfony\\Component\\Yaml\\' => array($vendorDir . '/symfony/yaml'),
+    'Symfony\\Component\\Filesystem\\' => array($vendorDir . '/symfony/filesystem'),
+    'Symfony\\Component\\DependencyInjection\\' => array($vendorDir . '/symfony/dependency-injection'),
+    'Symfony\\Component\\Config\\' => array($vendorDir . '/symfony/config'),
+    'Symfony\\Component\\ClassLoader\\' => array($vendorDir . '/symfony/class-loader'),
     'Psr\\Log\\' => array($vendorDir . '/psr/log'),
     'LazyMap\\' => array($vendorDir . '/ocramius/lazy-map/src'),
     'Instantiator\\' => array($vendorDir . '/ocramius/instantiator/src'),
