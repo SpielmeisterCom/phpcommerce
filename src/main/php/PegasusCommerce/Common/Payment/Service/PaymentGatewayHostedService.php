97ee51bda2d4961cc721a1be957e764841a7ef0f
Julian Haupt <julian.haupt@spielmeister.com>
2014-08-20 16:41:52 +0200

    added PaymentGatewayHostedService & PaymentGatewayReportingService

---
+++ src/main/php/PegasusCommerce/Common/Payment/Service/PaymentGatewayHostedService.php
@@ -0,0 +1,24 @@
+<?php
+namespace PegasusCommerce\Common\Payment\Service;
+use PegasusCommerce\Common\Payment\Dto\PaymentRequestDTO;
+use PegasusCommerce\Common\Payment\Dto\PaymentResponseDTO;
+use PegasusCommerce\Core\Payment\Service\Exception\PaymentException;
+
+/**
+ * <p>Certain Payment Integrations allow you to use a Hosted Solution,
+ * such as PayPal Express and SagePay Form.
+ * This API allows you to create the call to send a user to the Gateway's Hosted page
+ * and to capture and record transaction responses back from them.</p>
+ *
+ * @author Elbert Bautista (elbertbautista)
+ */
+interface PaymentGatewayHostedService {
+
+    /**
+     * @param PaymentRequestDTO $paymentRequestDTO
+     * @throws PaymentException
+     * @return PaymentResponseDTO
+     */
+    public function requestHostedEndpoint(PaymentRequestDTO $paymentRequestDTO);
+
+}
---
+++ src/main/php/PegasusCommerce/Common/Payment/Service/PaymentGatewayReportingService.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: julian
+ * Date: 20.08.14
+ * Time: 16:38
+ */
+
+namespace PegasusCommerce\Common\Payment\Service;
+use PegasusCommerce\Common\Payment\Dto\PaymentRequestDTO;
+use PegasusCommerce\Core\Payment\Service\Exception\PaymentException;
+
+/**
+ * <p>This API provides the ability to get the status of a Transaction after it has been submitted to the Gateway.
+ * Gateways have different ways to provide this information.
+ * For example, Cybersource can provide a nightly feed or FTP file that contain details of
+ * what was SETTLED, CHARGEBACK, etc... to be reconciled in your system.
+ * Braintree and Paypal have API hooks to either do a date based query or an individual
+ * inquiry on a particular transaction.</p>
+ *
+ * @author Elbert Bautista (elbertbautista)
+ */
+interface PaymentGatewayReportingService {
+    /**
+     * @throws PaymentException
+     * @return PaymentResponseDTO
+     */
+    public function findDetailsByTransaction(PaymentRequestDTO $paymentRequestDTO);
+
+} 
