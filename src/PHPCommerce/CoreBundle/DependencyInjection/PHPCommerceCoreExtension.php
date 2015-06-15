cbf651d17e03b026b7072044f116d7c6c267587f
Julian Haupt <julian.haupt@abfallscout.de>
2015-06-15 15:32:13 +0200

    the core bundle does not have any config yet

--- src/PHPCommerce/CoreBundle/DependencyInjection/PHPCommerceCoreExtension.php
+++ src/PHPCommerce/CoreBundle/DependencyInjection/PHPCommerceCoreExtension.php
@@ -11,7 +11,7 @@ class PHPCommerceCoreExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        //$loader->load('services.yml');
     }
 
     public function getAlias()
