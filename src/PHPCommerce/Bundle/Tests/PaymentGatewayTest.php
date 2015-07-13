695ebf1fe6e7076353e2c049c9083a887a9ebff4
Julian Haupt <julian.haupt@abfallscout.de>
2015-07-13 17:05:54 +0200

    fixed failing test

--- src/PHPCommerce/Bundle/Tests/PaymentGatewayTest.php
+++ src/PHPCommerce/Bundle/Tests/PaymentGatewayTest.php
@@ -4,22 +4,20 @@ namespace PHPCommerce\Bundle\Tests;
 use PHPCommerce\Payment\Dto\PaymentRequestDTO;
 use PHPCommerce\Payment\Service\PaymentGatewayConfigurationServiceProvider;
 use PHPUnit_Framework_TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class PaymentGatewayTest extends PHPUnit_Framework_TestCase {
+class PaymentGatewayTest extends KernelTestCase {
     /**
      * @var PaymentGatewayConfigurationServiceProvider
      */
     protected $paymentGatewayConfigurationServiceProvider;
 
     public function setUp() {
-        $container   = new ContainerBuilder();
-        $loader      = new YamlFileLoader($container, new FileLocator(array(__DIR__ . "/../Resources/config")));
-        $loader->load( 'config.yml');
-
-        $this->paymentGatewayConfigurationServiceProvider = $container->get('phpcommerce.payment.gateway.configuration_service_provider');
+        self::bootKernel();
+        $this->paymentGatewayConfigurationServiceProvider = static::$kernel->getContainer()->get('phpcommerce.payment.gateway.configuration_service_provider');
     }
 
     public function testNoPaymentConfigured() {
