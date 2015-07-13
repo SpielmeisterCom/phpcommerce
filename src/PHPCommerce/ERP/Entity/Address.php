f2156af7b1807cf3dd5974cfd8947c3cacd0eccb
Julian Haupt <julian.haupt@abfallscout.de>
2015-07-13 17:16:00 +0200

    Added Address and Country stubs

---
+++ src/PHPCommerce/ERP/Entity/Address.php
@@ -0,0 +1,102 @@
+<?php
+/*
+package org.broadleafcommerce.profile.core.domain;
+
+import java.io.Serializable;
+
+public interface Address extends Serializable {
+
+public void setId(Long id);
+
+public Long getId();
+
+public void setAddressLine1(String addressLine1);
+
+public String getAddressLine1();
+
+public void setAddressLine2(String addressLine2);
+
+public String getAddressLine2();
+
+public void setAddressLine3(String addressLine3);
+
+public String getAddressLine3();
+
+public void setCity(String city);
+
+public String getCity();
+
+public void setState(State state);
+
+public State getState();
+
+public void setPostalCode(String postalCode);
+
+public String getPostalCode();
+
+public String getCounty();
+
+public void setCounty(String county);
+
+public String getZipFour();
+
+public void setZipFour(String zipFour);
+
+public void setCountry(Country country);
+
+public Country getCountry();
+
+public String getTokenizedAddress();
+
+public void setTokenizedAddress(String tAddress);
+
+public Boolean getStandardized();
+
+public void setStandardized(Boolean standardized);
+
+public String getCompanyName();
+
+public void setCompanyName(String companyName);
+
+public boolean isDefault();
+
+public void setDefault(boolean isDefault);
+
+public String getFirstName();
+
+public void setFirstName(String firstName);
+
+public String getLastName();
+
+public void setLastName(String lastName);
+
+public void setFax(String fax);
+
+public Phone getPhonePrimary();
+
+public void setPhonePrimary(Phone phonePrimary);
+
+public Phone getPhoneSecondary();
+
+public void setPhoneSecondary(Phone phoneSecondary);
+
+public Phone getPhoneFax();
+
+public void setPhoneFax(Phone phone);
+
+public String getEmailAddress();
+
+public void setEmailAddress(String emailAddress);
+
+public boolean isBusiness();
+
+public void setBusiness(boolean isBusiness);
+
+public String getVerificationLevel();
+
+public void setVerificationLevel(String verificationLevel);
+
+public boolean isActive();
+
+public void setActive(boolean isActive);
+}*/
---
+++ src/PHPCommerce/ERP/Entity/Country.php
@@ -0,0 +1,16 @@
+/*
+package org.broadleafcommerce.profile.core.domain;
+
+import java.io.Serializable;
+
+public interface Country extends Serializable {
+
+public String getAbbreviation();
+
+public void setAbbreviation(String Abbreviation);
+
+public String getName();
+
+public void setName(String name);
+
+}*/
