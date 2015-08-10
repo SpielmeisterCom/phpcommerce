1e9dd1b33f7eb2be8b7036efd5481c7a752dd64f
Julian Haupt <julian.haupt@abfallscout.de>
2015-08-10 14:17:58 +0200

    autoload config.yml

--- src/PHPCommerce/Bundle/DependencyInjection/PHPCommerceExtension.php
+++ src/PHPCommerce/Bundle/DependencyInjection/PHPCommerceExtension.php
@@ -2,20 +2,40 @@
 namespace PHPCommerce\Bundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
 
-class PHPCommerceExtension extends Extension
+class PHPCommerceExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        //$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        //$loader->load('config.yml');
+
     }
 
     public function getAlias()
     {
         return 'php_commerce';
+    }
+
+    /**
+     * Allow an extension to prepend the extension configurations.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $bundles = $container->getParameter('kernel.bundles');
+
+        foreach(array('DoctrineMigrationsBundle', 'FrameworkBundle', 'DoctrineBundle') as $requiredBundle) {
+            if(!in_array($requiredBundle, array_keys($bundles))) {
+                throw new \Exception("Missing required Bundle " . $requiredBundle);
+            }
+        }
+
+        //prepend our own config
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('config.yml');
     }
 }
