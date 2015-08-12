6f8e353e8b716153d4cac066839f842d9a168fa3
Julian Haupt <jh@spielmeister.com>
2015-08-12 23:15:35 +0200

    support for env parameter in symfony console

--- app/console.php
+++ app/console.php
@@ -4,7 +4,17 @@ set_time_limit(0);
 require_once __DIR__.'/autoload.php';
 
 use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Debug\Debug;
 
-$kernel = new AppKernel('dev', true);
+$input = new ArgvInput();
+$env = $input->getParameterOption(array('--env', '-e'), getenv('SYMFONY_ENV') ?: 'dev');
+$debug = getenv('SYMFONY_DEBUG') !== '0' && !$input->hasParameterOption(array('--no-debug', '')) && $env !== 'prod';
+
+if ($debug) {
+    Debug::enable();
+}
+
+$kernel = new AppKernel($env, $debug);
 $application = new Application($kernel);
-$application->run();
+$application->run($input);
