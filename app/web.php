bf46bf12874c991f15a8c67063e21b707b0559a5
Julian Haupt <julian.haupt@hauptmedia.de>
2015-08-27 10:51:14 +0200

    added web directory

--- app/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Symfony\Component\HttpFoundation\Request;
-
-require_once __DIR__.'/autoload.php';
-
-$kernel = new AppKernel('prod', false);
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
