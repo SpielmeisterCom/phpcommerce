02170bb9664a7b72b9d9f5560db8c10938e9277b
Julian Haupt <julian.haupt@abfallscout.de>
2015-08-31 16:47:06 +0200

    added CheckoutService & CheckoutException

---
+++ src/PHPCommerce/Shop/Exception/CheckoutException.php
@@ -0,0 +1,35 @@
+<?php
+namespace PHPCommerce\Shop\Exception;
+
+class CheckoutException extends BroadleafException {
+/*
+    private CheckoutResponse checkoutResponse;
+
+    public CheckoutException() {
+        super();
+    }
+
+    public CheckoutException(String message, CheckoutSeed seed) {
+        super(message);
+        checkoutResponse = seed;
+    }
+
+        public CheckoutException(Throwable cause, CheckoutSeed seed) {
+        super(cause);
+            checkoutResponse = seed;
+    }
+
+        public CheckoutException(String message, Throwable cause, CheckoutSeed seed) {
+        super(message, cause);
+        checkoutResponse = seed;
+    }
+
+    public CheckoutResponse getCheckoutResponse() {
+        return checkoutResponse;
+    }
+
+    public void setCheckoutResponse(CheckoutResponse checkoutResponse) {
+        this.checkoutResponse = checkoutResponse;
+    }
+*/
+}
---
+++ src/PHPCommerce/Shop/Service/CheckoutService.php
@@ -0,0 +1,20 @@
+<?php
+namespace PHPCommerce\Shop\Service;
+
+interface CheckoutService {
+
+    /**
+     * <p>Checks out an order by executing the blCheckoutWorkflow. The <b>order</b> is saved both before and after the workflow
+     * is executed so that activities can modify the various entities on and related to the <b>order</b>.</p>
+     *
+     * <p>This method is also thread-safe; 2 requests cannot attempt to check out the same <b>order</b></p>
+     *
+     * @param order the order to be checked out
+     * @return CheckoutResponse
+     * @throws CheckoutException if there are any exceptions while executing any of the activities in the workflow (assuming
+     * that the workflow does not already have a preconfigured error handler) or if the given <b>order</b> has already been
+     * checked out (in Broadleaf terms this means the <b>order</b> has already been changed to {@link OrderStatus#SUBMITTED})
+     */
+    public function performCheckout(Order $order);
+
+}
