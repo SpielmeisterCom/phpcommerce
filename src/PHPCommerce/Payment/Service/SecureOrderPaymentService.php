7dfbf8dde378e9fea9596e128a18c39500c2d757
Julian Haupt <julian.haupt@abfallscout.de>
2015-08-31 15:43:33 +0200

    added SecureOrderPaymentService interface

---
+++ src/PHPCommerce/Payment/Service/SecureOrderPaymentService.php
@@ -0,0 +1,42 @@
+<?php
+
+use PHPCommerce\Entity\Secure\Referenced;
+use PHPCommerce\Payment\PaymentType;
+
+interface SecureOrderPaymentService {
+
+    /**
+     * @param $referenceNumber
+     * @param PaymentType $paymentType
+     * @throws WorkflowException
+     * @return Referenced
+     */
+    public function findSecurePaymentInfo($referenceNumber, PaymentType $paymentType);
+
+    /**
+‚     * @param Referenced $securePayment
+     * @return Referenced
+     */
+    public function save(Referenced $securePayment);
+
+    /**
+     * @param PaymentType $paymentType
+     * @return Referenced
+     */
+    public function create(PaymentType $paymentType);
+
+    /**
+     * @param Referenced $securePayment
+     * @return mixed
+     */
+    public function remove(Referenced $securePayment);
+
+    /**
+     * @param $referenceNumber
+     * @param PaymentType $paymentType
+     * @return mixed
+     * @internal param $refernceNumber
+     */
+    public function findAndRemoveSecurePaymentInfo($referenceNumber, PaymentType $paymentType);
+
+}
