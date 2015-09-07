c20b3a7922b369d48ccfe292fa50fbdb665573ed
Julian Haupt <julian.haupt@abfallscout.de>
2015-09-07 10:36:02 +0200

    added comment to NullOrder

--- src/PHPCommerce/ERP/Entity/NullOrder.php
+++ src/PHPCommerce/ERP/Entity/NullOrder.php
@@ -6,6 +6,14 @@ use PHPCommerce\ERP\Domain\OrderInterface;
 use PHPCommerce\ERP\Domain\OrderPayment;
 use PHPCommerce\Payment\Entity\Payment;
 
+/**
+ * NullOrderImpl is a class that represents an unmodifiable, empty order. This class is used as the default order
+ * for a customer. It is a shared class between customers, and serves as a placeholder order until an item
+ * is initially added to cart, at which point a real Order gets created. This prevents creating individual orders
+ * for customers that are just browsing the site.
+ *
+ * @author apazzolini
+ */
 class NullOrder implements OrderInterface
 {
 
