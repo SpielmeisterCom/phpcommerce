f7df7a7bace6a467926cc07d240e14413e4fd3e7
Julian Haupt <julian.haupt@abfallscout.de>
2015-09-07 15:43:45 +0200

    Renamed TextControllerTest to CheckoutControllerTest

---
+++ src/PHPCommerce/Shop/Tests/CheckoutControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace PHPCommerce\Bundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class CheckoutControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+//        $client = static::createClient();
+//        $crawler = $client->request('GET', '/test');
+//        $this->assertGreaterThan(0, $crawler->filter('html:contains("hello world")')->count());
+    }
+}
